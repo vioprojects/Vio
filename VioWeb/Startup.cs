5366d642df1e943c2587ef7a1854dc90a36cfbd1
Dinh <dovandinh012@gmail.com>
2016-08-11 16:40:58 +0700

    Khoi tao project

---
+++ VioWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VioWeb.Startup))]
+namespace VioWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
